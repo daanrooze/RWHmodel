3d7e4b28376f4d6fb61018632e70dba289fda298
Tjalling <tjalling.dejong@deltares.nl>
2024-07-30 14:11:15 +0200

    added docstring

--- RWHmodel/utils.py
+++ RWHmodel/utils.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
 
+def convert_m3_to_mm(df: pd.DataFrame, col: str, surface_area: float) -> pd.DataFrame:
+    """Convert a cubic meters pd.series to milimeters.
 
+    Parameters
+    ----------
+    df : pd.DataFrame
+    col : str
+        name of column in cubic meters
+    surface_area : float
+        surface area in meters squared
 
-def convert_m3_to_mm(df: pd.DataFrame , col: str , surface_area: float) -> pd.DataFrame:
-        df[col] = df[col] / surface_area
-        return df 
-         
+    Returns
+    -------
+    pd.DataFrame
+    """
+    df[col] = df[col] / surface_area
+    return df
